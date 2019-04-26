cc12bfd8d1db8ff931ab23d5110e06c3da51a742
bilgeadammelike <bilgeadammelike@mynet.com>
2019-04-26 11:27:24 +0300

    Ders 2 LINq ile sorgulama

---
+++ OgrenciTakip.DAL/ViewModel/OgrenciVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OgrenciTakip.DAL.ViewModel
+{
+   public class OgrenciVM
+    {
+        public string SinifDegeri { get; set; }
+        public string OgrenciIsimDegeri { get; set; }
+    }
+}
--- OgrenciTakip.UI/Form2.cs
+++ OgrenciTakip.UI/Form2.cs
@@ -1,4 +1,5 @@
 ﻿using OgrenciTakip.DAL;
+using OgrenciTakip.DAL.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -26,7 +27,25 @@ namespace OgrenciTakip.UI
         private void SinifYukle()
         {
             OgrenciTakipDBEntities db = new OgrenciTakipDBEntities();
-            db.Sinif.ToList();
+            listBox1.DataSource= db.Sinif.ToList();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Sinif tiklanilan = listBox1.SelectedItem as Sinif;
+
+            OgrenciTakipDBEntities db = new OgrenciTakipDBEntities();
+            var deger = (from sinif in db.Sinif
+                        join sinifOgrenci in db.SinifOgrenci on sinif.SinifID equals sinifOgrenci.SinifID
+                        join ogrenci in db.Ogrenci on sinifOgrenci.OgrenciID equals ogrenci.OgrenciID
+                        select new OgrenciVM() {
+                            SinifDegeri =sinif.SinifAdi,
+                            OgrenciIsimDegeri =ogrenci.AdiSoyadi /*sinifOgrenci.OgrenciID.ToString()*/
+
+                        }).ToList();
+
+
+
         }
     }
 }
